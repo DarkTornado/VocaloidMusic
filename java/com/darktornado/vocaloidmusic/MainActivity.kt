a8167b3cc4694951e43a6216bbf0ec7f1d0a32a9
Dark Tornado <dt3141592@naver.com>
2021-12-24 23:59:11 +0900

    Update MainActivity.kt

--- java/com/darktornado/vocaloidmusic/MainActivity.kt
+++ java/com/darktornado/vocaloidmusic/MainActivity.kt
@@ -2,9 +2,14 @@ package com.darktornado.vocaloidmusic
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val layout = LinearLayout(this)
+        layout.orientation = 1
+
+        setContentView(layout)
     }
 }
