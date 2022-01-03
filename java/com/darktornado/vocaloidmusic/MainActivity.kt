282937f150d5fa90b26fab0e4109c452362d771d
Dark Tornado <dt3141592@naver.com>
2022-01-03 18:44:58 +0900

    정?보

--- java/com/darktornado/vocaloidmusic/MainActivity.kt
+++ java/com/darktornado/vocaloidmusic/MainActivity.kt
@@ -3,6 +3,9 @@ package com.darktornado.vocaloidmusic
 import android.app.Activity
 import android.os.Bundle
 import android.widget.LinearLayout
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,4 +15,28 @@ class MainActivity : Activity() {
 
         setContentView(layout)
     }
+
+    private fun getSongArray(): Array<SongInfo?>? {
+        try {
+            val isr = InputStreamReader(assets.open("songs.csv"))
+            val br = BufferedReader(isr)
+            var str = br.readLine()
+            var line = ""
+            while (br.readLine().also { line = it } != null) {
+                str += "\n$line"
+            }
+            isr.close()
+            br.close()
+            val cache = str.split("\n").toTypedArray()
+            val result = arrayOfNulls<SongInfo>(cache.size)
+            for (n in cache.indices) {
+                result[n] = SongInfo(cache[n])
+            }
+            Arrays.sort(result)
+            return result
+        } catch (e: Exception) {
+        }
+        return null
+    }
+
 }
---
+++ java/com/darktornado/vocaloidmusic/SongInfo.kt
@@ -0,0 +1,18 @@
+package com.darktornado.vocaloidmusic
+
+class SongInfo (input: String) {
+    val title: String
+    val ky: String
+    val tj: String
+    val vocal: String
+    val prod: String
+
+    init {
+        val data = input.split("    ")
+        ky = data[0]
+        tj = data[1]
+        title = data[2]
+        vocal = data[3]
+        prod = data[4]
+    }
+}
