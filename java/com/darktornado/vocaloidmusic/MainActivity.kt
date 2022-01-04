452c3d9079a193b2c2ebef2378d20aecc546afff
Dark Tornado <dt3141592@naver.com>
2022-01-04 23:28:14 +0900

    몰?루

--- java/com/darktornado/vocaloidmusic/MainActivity.kt
+++ java/com/darktornado/vocaloidmusic/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.darktornado.vocaloidmusic
 
+import android.R
 import android.app.Activity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
@@ -12,6 +14,18 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         val layout = LinearLayout(this)
         layout.orientation = 1
+        val songs = getSongArray()!!
+        val names = arrayOfNulls<String>(songs.size)
+        for (n in songs.indices) {
+            names[n] = songs[n]!!.title
+        }
+
+        val list = ListView(this)
+        list.adapter = ArrayAdapter<Any?>(this, R.layout.simple_list_item_1, names)
+        list.isFastScrollEnabled = true
+        list.onItemClickListener = OnItemClickListener { parent, view, pos, id ->
+            Toast.makeText(this, songs[pos]!!.title, 1).show()
+        }
 
         setContentView(layout)
     }
